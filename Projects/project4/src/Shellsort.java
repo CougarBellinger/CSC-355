61f07732a2440c43b2211c0a8953bd3049222c9b
cougarbellinger <cougarbellinger@arizona.edu>
2025-03-27 10:43:45 -0700

    Setup Test Sort for file input and function output

--- Projects/project4/src/Shellsort.java
+++ Projects/project4/src/Shellsort.java
@@ -1,7 +1,15 @@
 class Shellsort {
-    // Sorts the array using shell sort
-    public static void sort() {
+    private static Integer[] data;
+    private static Integer[] sortedData;
 
+    // Sorts the array using shell sort
+    public static void sort(Integer[] inputData) {
+        data = inputData;
+
+    }
+
+    public static Integer[] getSorted() {
+        return sortedData;
     }
 
 }
--- Projects/project4/src/TestSort.java
+++ Projects/project4/src/TestSort.java
@@ -1,16 +1,37 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TestSort {
     public static void main(String[] args) {
         Scanner scnnr = new Scanner(System.in);
-        String fileName = "../data/array1.txt";
-        try (FileReader reader = new FileReader(fileName)) {
+        String fileName = "../data/testArray1.txt";
+
+        // sort the array and store it in Shellsort instance
+        Shellsort.sort(fileArray(fileName));
+        // print out the shellsort sorted array using to string
+        System.out.println(Arrays.toString(Shellsort.getSorted()));
+    }
+
+    public static Integer[] fileArray(String fileName) {
+        ArrayList<Integer> inputArray = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                inputArray.add(Integer.parseInt(line));
+            }
 
         } catch (IOException e) {
             System.err.println(e.getMessage());
+            System.exit(0);
         }
 
+        Integer[] data = new Integer[inputArray.size()];
+        return inputArray.toArray(data);
     }
 }
